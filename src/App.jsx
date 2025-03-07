c8a584e370e87f8180a6cdcc69783cb7d12b42d9
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-03-07 10:43:45 +0200

    react hw4

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import ContactList from "./components/ContactList/ContactList";
-import initialContacts from "./contacts.json";
-import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
+import axios from "axios";
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem("contacts");
-    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
-  });
-  const [filter, setFilter] = useState("");
-
-  useEffect(() => {
-    if (contacts.length > 0) {
-      localStorage.setItem("contacts", JSON.stringify(contacts));
-    }
-  }, [contacts]);
-
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  const deleteContact = (contactId) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((contact) => contact.id !== contactId);
-    });
-  };
-
-  return (
-    <div className="App">
-      <h1>Phonebook</h1>
-      <ContactForm
-        addContact={(newContact) => setContacts([...contacts, newContact])}
-      />
-      <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
-    </div>
-  );
+  return;
 };
 
 export default App;

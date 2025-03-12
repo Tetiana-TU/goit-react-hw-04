55e8bae0f3de48be1b024f02bc1597a75ffecd1b
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-03-12 14:38:51 +0200

    hw4react

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import css from "./LoadMoreBtn.module.css";
 
 const LoadMoreBtn = ({ handleLoadMore, isActive }) => {
   return (
-    <button
-      className={css.loadmorebtn}
-      onClick={handleLoadMore}
-      type="button"
-      disabled={isActive}
-    >
+    <button onClick={handleLoadMore} type="button" disabled={isActive}>
       Load more
     </button>
   );
